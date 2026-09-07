63fc3966e107148d4d61beb2b8d768d5c1358d08
PaulaBeni <paulam.beni@gmail.com>
2026-09-07 13:31:26 -0300

    Agrego logos y correccion de errores

--- components/load-components.js
+++ components/load-components.js
@@ -5,7 +5,7 @@ async function cargarComponente(url, idDestino) {
 }
 
 async function iniciar() {
-  await cargarComponente('/components/header.html', 'header-placeholder');
+  await cargarComponente('components/header.html', 'header-placeholder');
   initBurgerMenu();
   initNavScroll();
 }
